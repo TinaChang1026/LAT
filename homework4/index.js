a83c278db9f9a9eacacb0697d27b30190f0e9895
TinaChang1026 <yuting200226@gmail.com>
2023-05-03 18:56:58 +0800

    test

---
+++ homework4/index.js
@@ -0,0 +1,70 @@
+'use strict';
+const line = require('@line/bot-sdk'),
+      express = require('express'),
+      configGet = require('config');
+const {TextAnalyticsClient, AzureKeyCredential} = require("@azure/ai-text-analytics");
+
+//Line config
+const configLine = {
+  channelAccessToken:configGet.get("CHANNEL_ACCESS_TOKEN"),
+  channelSecret:configGet.get("CHANNEL_SECRET")
+};
+
+//Azure Text Sentiment
+const endpoint = configGet.get("ENDPOINT");
+const apiKey = configGet.get("TEXT_ANALYTICS_API_KEY");
+
+const client = new line.Client(configLine);
+const app = express();
+
+const port = process.env.PORT || process.env.port || 3001;
+
+app.listen(port, ()=>{
+  console.log(`listening on ${port}`);
+   
+});
+
+async function MS_TextSentimentAnalysis(thisEvent){
+    console.log("[MS_TextSentimentAnalysis] in");
+    const analyticsClient = new TextAnalyticsClient(endpoint, new AzureKeyCredential(apiKey));
+    let documents = [];
+    documents.push(thisEvent.message.text);
+    
+    const results = await analyticsClient.analyzeSentiment(documents);
+    console.log("[results] ", JSON.stringify(results));
+
+    const sentimentScore = results[0].confidenceScores[results[0].sentiment];
+    const sentimentText = results[0].sentiment === "positive" ? "正向" :
+                          results[0].sentiment === "negative" ? "負向" :
+                          "中性";
+
+    const echo = {
+      type: 'text',
+      text: `${sentimentText}。分數： ${sentimentScore.toFixed(2)}`
+      
+    };
+
+    return client.replyMessage(thisEvent.replyToken, echo);
+
+}
+
+app.post('/callback', line.middleware(configLine),(req, res)=>{
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result)=>res.json(result))
+    .catch((err)=>{
+      console.error(err);
+      res.status(500).end();
+    });
+});
+
+function handleEvent(event){
+  if(event.type !== 'message' || event.message.type !== 'text'){
+    return Promise.resolve(null);
+  }
+
+  MS_TextSentimentAnalysis(event)
+    .catch((err) => {
+      console.error("Error:", err);
+    }); 
+}
